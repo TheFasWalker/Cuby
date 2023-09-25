83c1d667a00f970cd4e6047bd46b523e42986373
Fast <krinickij.v.a@gmail.com>
2023-09-25 11:12:41 +0300

    js foiirm validation

--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -11,6 +11,7 @@ function fixBody() {
   body.style.overflow = 'hidden';
   body.style.paddingRight = `${paddingRightt}px`;
   header.style.marginRight = `${paddingRightt}px`;
+  console.log('fix body');
 }
 function removeFixBody() {
   document.querySelector('body').style.overflow = '';
@@ -28,7 +29,59 @@ function closeMobileMenu() {
     removeFixBody();
   }
 }
+function openCallBackPopUp() {
+  let popupForm = document.querySelector('.js-popup-form');
+  if (popupForm) {
+    popupForm.classList.add('popup__wrapper_active');
+    fixBody();
+  }
+}
+function closePopup() {
+  let activeFormPopup = document.querySelector('.popup__wrapper_active');
+  if (activeFormPopup) {
+    activeFormPopup.classList.remove('popup__wrapper_active');
+    removeFixBody();
+  }
+}
+function checkCheckbox(formNode) {
+  let checkbox = formNode.querySelector('.checkbox');
+  let checkboxParentNode = checkbox.parentNode;
+  if (checkbox.checked) {
+    if (checkboxParentNode.classList.contains('error')) {
+      checkboxParentNode.classList.remove('error');
+    }
+    return true;
+  } else {
+    checkboxParentNode.classList.add('error');
+  }
+}
+function validateForm(formForValidation) {
+  if (checkCheckbox(formForValidation)) {
+    return true;
+  }
+}
+function showThanksPopup() {
+  document.querySelector('.js-popup-thanks').classList.add('popup__wrapper_active');
+  fixBody();
+  setTimeout(closePopup, 3000);
+}
+function submitForm(formNode) {
+  formNode.querySelectorAll('input').forEach(input => {
+    input.value = '';
+  });
+  if (formNode.querySelector('textarea')) {
+    formNode.querySelector('textarea').value = '';
+  }
+}
 document.addEventListener("DOMContentLoaded", () => {
+  const div = document.querySelector('.popup__container');
+  document.addEventListener('click', e => {
+    const withinBoundaries = e.composedPath().includes(div);
+    const openButton = e.composedPath().includes(document.querySelector('.js-button-call-form'));
+    if (!withinBoundaries && !openButton) {
+      closePopup();
+    }
+  });
   document.querySelector('.js-header-mobile-menu-button').addEventListener('click', showMobileMenu);
   document.querySelector('.js-close-mobile-menu').addEventListener('click', closeMobileMenu);
   const tuningSwiper = new Swiper('.js-tuning-swiper', {
@@ -36,5 +89,27 @@ document.addEventListener("DOMContentLoaded", () => {
     spaceBetween: 10,
     loop: false,
     centeredSlides: false
+  });
+  document.querySelectorAll('.js-button-call-form').forEach(button => {
+    button.addEventListener('click', openCallBackPopUp);
+  });
+  document.querySelectorAll('.js-popup-close-button').forEach(btn => {
+    btn.addEventListener('click', closePopup);
+  });
+  document.querySelectorAll('form').forEach(form => {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      if (validateForm(form)) {
+        if (true) {
+          //сюда вставлять валидацию на бота - всё упаковать в отдельную функцию с возможным появлением 2й рекапчи
+          console.log('submit');
+          submitForm(form);
+          if (document.querySelector('.popup__wrapper_active')) {
+            closePopup();
+          }
+          showThanksPopup();
+        }
+      }
+    });
   });
 });
